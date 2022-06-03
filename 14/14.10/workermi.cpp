35ed29fc9cb4d6f1ad9e52716977220870f05eae
zzz-00 <576423404@qq.com>
2022-06-03 13:29:24 +0800

    14

---
+++ 14/14.10/workermi.cpp
@@ -0,0 +1,120 @@
+// workermi.cpp -- working class methods with MI
+#include "workermi.h"
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+// Worker methods
+Worker::~Worker() {}
+
+// protected methods
+void Worker::Data() const
+{
+    cout << "Name: " << fullname << endl;
+    cout << "Employee ID: " << id << endl;
+}
+
+void Worker::Get()
+{
+    getline(cin, fullname);
+    cout << "Enter worker's ID: ";
+    cin >> id;
+    while (cin.get() != '\n')
+        continue;
+}
+
+// Waiter methods
+void Waiter::Set()
+{
+    cout << "Enter waiter's name: ";
+    Worker::Get();
+    Get();
+}
+
+void Waiter::Show() const
+{
+    cout << "Category: waiter\n";
+    Worker::Data();
+    Data();
+}
+
+// protect methods
+void Waiter::Data() const
+{
+    cout << "Panache rating: " << panache << endl;
+}
+
+void Waiter::Get()
+{
+    cout << "Enter waiter's panache rating: ";
+    cin >> panache;
+    while (cin.get() != '\n')
+        continue;
+}
+
+// Singer methods
+std::string Singer::pv[Singer::Vtypes] = {"other", "alto", "contralto", "soprano", "bass", "baritone", "tenor"};
+
+void Singer::Set()
+{
+    cout << "Enter singer's name: ";
+    Worker::Get();
+    Get();
+}
+
+void Singer::Show() const
+{
+    cout << "Category: singer\n";
+    Worker::Data();
+    Data();
+}
+
+// protect methods
+void Singer::Data() const
+{
+    cout << "Vocal range: " << pv[voice] << endl;
+}
+
+void Singer::Get()
+{
+    cout << "Enter number for singer's vocal range:\n";
+    int i;
+    for (i = 0; i < Vtypes; i++)
+    {
+        cout << i << ": " << pv[i] << "    ";
+        if (i % 4 == 3)
+            cout << endl;
+    }
+    if (i % 4 != 0)
+        cout << endl;
+    cin >> voice;
+    while (cin.get() != '\n')
+        continue;
+}
+
+// SingingWaiter methods
+void SingingWaiter::Data() const
+{
+    Singer::Data();
+    Waiter::Data();
+}
+
+void SingingWaiter::Get()
+{
+    Waiter::Get();
+    Singer::Get();
+}
+
+void SingingWaiter::Set()
+{
+    cout << "Enter singing waiter's name: ";
+    Worker::Get();
+    Get();
+}
+
+void SingingWaiter::Show() const
+{
+    cout << "Category: singing waiter" << endl;
+    Worker::Data();
+    Data();
+}
