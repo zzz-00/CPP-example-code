cf0be09f1caa2e5c02f23d0f8635a507ac8cff92
zzz-00 <576423404@qq.com>
2022-08-03 23:18:11 +0800

    main

--- 02/02.01myfirst/myfirst.cpp
+++ 02/02.01myfirst/myfirst.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main()
 {
-    cout << "Com up and C++ me some time.";
+    cout << "Come up and C++ me some time.";
     cout << endl;
     cout << "You won't regret it!" << endl;
     cout << "git test" << endl;
